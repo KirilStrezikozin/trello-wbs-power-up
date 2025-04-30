b162deb5ba9a17c7032197be5ca22482a348524e
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-04-30 12:00:58 +0200

    .*: change board-button to callback. add assemble list data
    * Change board-button capability handler from a URL-like tocallback-like
    to read board lists and cards every time the power-up'sboard-button is
    pressed.
    * Add collect and assemble the data required for WBS from read
    Trellolists and cards.
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- src/app/lib/power-up.ts
+++ src/app/lib/power-up.ts
@@ -6,7 +6,9 @@
  * You may not use this file except in compliance with the MIT license terms.
  */
 
+import { Trello } from '@/src/types/trello';
 import { PowerUp } from '@/src/types/power-up';
+
 import { LogoIcon, PowerUpName } from './constants';
 
 /**
@@ -17,6 +19,21 @@ export enum PowerUpState {
   Ready,
   NotInTrelloError,
   UnknownError,
+}
+
+/**
+ * Assemble the data required to build a work breakdown structure chart.
+ * @param lists - Trello Lists to get the data from.
+ * @returns Assembled data.
+ */
+function makeData(lists: Trello.PowerUp.List[]): PowerUp.ListData[] {
+  return lists.map(({ name, cards }) => ({
+    name,
+    cards: cards.map(({ name, dueComplete }) => ({
+      name,
+      isComplete: dueComplete,
+    })),
+  }));
 }
 
 export const CapabilityHandlers: PowerUp.CapabilityHandlers = {
@@ -33,6 +50,18 @@ export const CapabilityHandlers: PowerUp.CapabilityHandlers = {
       text: PowerUpName,
       condition: 'always',
       url: callbackUrl,
+
+      callback(t: Trello.PowerUp.IFrame) {
+        t.lists('all').then((lists: Trello.PowerUp.List[]) => {
+          const data = makeData(lists);
+          console.log(data.length, JSON.stringify(data, null, 2));
+        });
+
+        return t.alert({
+          message: 'WBS chart will open in a new browser tab',
+          duration: 10,
+        });
+      },
     }
   }
 }
--- src/types/power-up.d.ts
+++ src/types/power-up.d.ts
@@ -16,7 +16,33 @@ namespace PowerUp {
      * @param origin - A string representing the power-up origin URL.
      * @returns Trello board-button capability handler.
      */
-    getDefault: (origin: string) => Trello.PowerUp.BoardButtonUrl;
+    getDefault: (origin: string) => Trello.PowerUp.BoardButtonCallback;
+  }
+
+  /**
+   * Represents the data from a single Trello List required to build a work
+   * breakdown structure chart.
+   */
+  export interface ListData {
+    /**
+     * Name of the Trello List.
+     */
+    name: string,
+
+    /**
+     * Array of card information.
+     */
+    cards: {
+      /**
+       * Name of the Trello Card.
+       */
+      name: string,
+
+      /**
+       * Whether the Trello Card has been completed.
+       */
+      isComplete: boolean,
+    }[],
   }
 
 }
